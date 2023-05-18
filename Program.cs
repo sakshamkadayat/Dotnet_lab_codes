b0a272e77c8f5709ca912e5821863b85c9091b1c
sakshamkadayat <sakshamkadayat@gmail.com>
2023-05-18 18:02:30 +0545

    Dotnet lab codes

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Saksham
+{
+    class defaultconstructor
+    {
+        int labno;
+        string name;
+        int rollno;
+        
+        public defaultconstructor()
+        {
+            Console.WriteLine("Default constructor is called");
+            labno = 1;
+            name = "saksham kadayat";
+            rollno = 23980;
+        }
+        
+        static void Main(string[] args)
+        {
+            defaultconstructor sak = new defaultconstructor();
+            Console.WriteLine("labno: " + sak.labno);
+            Console.WriteLine("Name: " + sak.name);
+            Console.WriteLine("Rollno: " + sak.rollno);
+        }
+    }
+}
